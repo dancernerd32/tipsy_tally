3c2779f7b828a2eabd6dce1d15d1264327ac95e7
graffam <graffam87@gmail.com>
2014-12-30 10:05:44 -0500

    add travis CI

--- spec/features/user_adds_a_new_drink_spec.rb
+++ spec/features/user_adds_a_new_drink_spec.rb
@@ -1,9 +1,10 @@
 feature "user adds a new drink" do
 # As a user, I want to be able to create a new drink, so that it can be
 # reviewed and viewed in
+
 # Acceptance Critieria:
-# [] Must specify either alcoholic or non alcoholic
 # [] Must specify a name
+# [] Must specify either alcoholic or non alcoholic
 # [] If alcoholic, a list of alcohols should apear, allowing the user to pick several
 # [] I can only input drink names that have at least 3 letters.
 # [] I must input a description
