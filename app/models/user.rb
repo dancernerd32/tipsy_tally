208438028df99c73ca1906f9b4b44800e33f8f65
dancernerd32 <dancernerd32@gmail.com>
2014-12-31 13:16:07 -0500

    Adds avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,12 @@
 class User < ActiveRecord::Base
+
+  mount_uploader :avatar, AvatarUploader
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
         #  :authentication_keys => [:login]
-  attr_accessor :login
+   attr_accessor :login
 
 
   def self.find_for_database_authentication(warden_conditions)
@@ -21,6 +23,10 @@ class User < ActiveRecord::Base
   :uniqueness => {
     :case_sensitive => false
   }
+
+  validates_integrity_of  :avatar
+  validates_processing_of :avatar
+  
 
   # or if you will use this variable somwhere else in the code:
   # def login=(login)
