b91a9966c2808d875faaf2025ab5348d04a8a12f
dancernerd32 <dancernerd32@gmail.com>
2014-12-31 15:03:59 -0500

    Adds test for invalid credentials

--- spec/features/user_signs_in_spec.rb
+++ spec/features/user_signs_in_spec.rb
@@ -32,11 +32,40 @@ feature 'user signs in', %Q{
     expect(page).to have_content('Signed in successfully')
     expect(page).to have_content('Sign Out')
   end
-  scenario 'specify invalid credentials' do
+  scenario 'specify invalid credentials - empty fields' do
     visit new_user_session_path
 
     click_button 'Log in'
     expect(page).to have_content('Invalid login or password')
     expect(page).to_not have_content('Sign Out')
   end
+
+  scenario 'specify invalid credentials - email/password combo not in database' do
+
+    user = FactoryGirl.create(:user)
+
+    visit new_user_session_path
+
+    fill_in 'Login', with: user.email
+    fill_in 'Password', with: "Foobar1235"
+
+    click_on 'Log in'
+    expect(page).to have_content('Invalid login or password')
+
+  end
+
+  scenario 'specify invalid credentials - username/password combo not in database' do
+
+
+    user = FactoryGirl.create(:user)
+
+    visit new_user_session_path
+
+    fill_in 'Login', with: user.username
+    fill_in 'Password', with: "Foobar1235"
+
+    click_on 'Log in'
+    expect(page).to have_content('Invalid login or password')
+  end
+
 end
