9c942ee20a8680b88b29d4ebf35f46177d2c95ca
dancernerd32 <dancernerd32@gmail.com>
2015-01-04 12:56:20 -0500

    change attempted for hound

--- db/migrate/20150102191126_add_default_avatar_url.rb
+++ db/migrate/20150102191126_add_default_avatar_url.rb
@@ -1,7 +1,9 @@
 class AddDefaultAvatarUrl < ActiveRecord::Migration
   def change
     change_column
-    :users, :avatar, :string,
-    default: "https://s3-us-west-2.amazonaws.com/tipsy-tally/default.jpg"
+      :users,
+      :avatar,
+      :string,
+      default: "https://s3-us-west-2.amazonaws.com/tipsy-tally/default.jpg"
   end
 end
