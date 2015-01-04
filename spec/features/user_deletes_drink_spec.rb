8cdcdb5493da9d4116a21449f2373f975ddbeda7
Cori Snedecor <coriannas@yahoo.com>
2015-01-04 17:51:22 -0500

    Adds more tests for deleting drinks

--- spec/features/user_deletes_drink_spec.rb
+++ spec/features/user_deletes_drink_spec.rb
@@ -7,12 +7,12 @@ feature "User deletes a drink", %{
   } do
 
   # Acceptance Criteria:
-  # [] I must be logged in
-  # [] I can only delete my own drinks
-  # [] When I delete a drink, I am redirected to the drinks index page and given
-  #    a success message
-  # [] If I am not logged in, I cannot see the option to delete a drink
-  # [] If the drink is not mine, I cannot see the option to delete the drink
+  # [X] I must be logged in
+  # [X] I can only delete my own drinks
+  # [X] When I delete a drink, I am redirected to the drinks index page and given
+  #     a success message
+  # [X] If I am not logged in, I cannot see the option to delete a drink
+  # [X] If the drink is not mine, I cannot see the option to delete the drink
 
   context "User is logged in" do
     before(:each) do
@@ -28,7 +28,7 @@ feature "User deletes a drink", %{
     end
 
 
-    scenario "User deletes their own drink" do
+    scenario "User deletes their own alcoholic drink" do
       drink = FactoryGirl.create(:alcohol_drink, user: @user1)
 
       visit drink_path(drink)
@@ -36,6 +36,54 @@ feature "User deletes a drink", %{
 
       expect(page).to have_content "Successfully deleted drink"
       expect(page).not_to have_content drink.name
+    end
+
+    scenario "User looks for delete option for someone else's
+    alchoholic drink" do
+      user2 = FactoryGirl.create(:user)
+      drink = FactoryGirl.create(:alcohol_drink, user: user2)
+
+      visit drink_path(drink)
+
+      expect(page).not_to have_content "Delete Drink"
+    end
+
+    scenario "User deletes their own non-alcoholic drink" do
+      drink = FactoryGirl.create(:non_alcohol_drink, user: @user1)
+
+      visit drink_path(drink)
+      click_on "Delete Drink"
+
+      expect(page).to have_content "Successfully deleted drink"
+      expect(page).not_to have_content drink.name
+    end
+
+    scenario "User looks for delete option for someone else's
+    non-alchoholic drink" do
+      user2 = FactoryGirl.create(:user)
+      drink = FactoryGirl.create(:non_alcohol_drink, user: user2)
+
+      visit drink_path(drink)
+
+      expect(page).not_to have_content "Delete Drink"
+    end
+  end
+
+  context "User is not logged in" do
+    scenario "User looks for delete option on a non-alcoholic drink" do
+      drink = FactoryGirl.create(:non_alcohol_drink)
+
+      visit drink_path(drink)
+
+      expect(page).not_to have_content "Delete Drink"
+    end
+
+    scenario "User looks for delete option on a alcoholic drink" do
+      drink = FactoryGirl.create(:alcohol_drink)
+
+      visit drink_path(drink)
+
+      expect(page).not_to have_content "Delete Drink"
     end
   end
 end
