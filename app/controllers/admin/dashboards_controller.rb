14c5867d99953b51335f8b0cba51b30717c6c968
Cori Snedecor <coriannas@yahoo.com>
2015-01-06 16:13:12 -0500

    Refactoring admin validations

--- app/controllers/admin/dashboards_controller.rb
+++ app/controllers/admin/dashboards_controller.rb
@@ -2,12 +2,14 @@ module Admin
   class DashboardsController < ApplicationController
 
     before_filter :authenticate_user!
-
-    def show
+    before_filter do
       if current_user.role.downcase != "admin"
         flash[:error] = "You are not authorized to view that page"
         redirect_to root_path
       end
+    end
+
+    def show
     end
   end
 end
--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,14 +2,15 @@ module Admin
   class UsersController < ApplicationController
 
     before_filter :authenticate_user!
-
-    def index
-      @users = User.all.order(username: :asc)
-
+    before_filter do
       if current_user.role.downcase != "admin"
         flash[:error] = "You are not authorized to view that page"
         redirect_to root_path
       end
+    end
+
+    def index
+      @users = User.all.order(username: :asc)
     end
 
   end
