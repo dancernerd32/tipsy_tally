f0b649e39b76f1d3ff8333c5c8ec50dc6cedfbf4
graffam <graffam87@gmail.com>
2015-01-07 11:31:53 -0500

    Fixed drink destroy now actually destroys drink, added drink index page

---
+++ spec/features/user_views_all_drinks_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+
+feature "visitor views drinks", %{
+  As a visitor,
+  I want to be able to view all drinks
+  So I can decide which drink to look at
+  } do
+
+    # Acceptance Criteria:
+    # [X] Vistor can see all the drinks listed on the homepage
+    # [X] If I click a drink, it should bring me to the drinks details page
+
+    scenario "vistor views the drinks and click on one" do
+      drink = FactoryGirl.create(:drink, description: "This drink is awesome!")
+      drink1 = FactoryGirl.create(:drink)
+
+      visit drinks_path
+
+      expect(page).to have_content(drink.name)
+      expect(page).to have_content(drink1.name)
+
+      click_on drink.name
+
+      expect(page).to have_content(drink.liquors.first)
+      expect(page).to have_content("This drink is awesome!")
+    end
+  end
