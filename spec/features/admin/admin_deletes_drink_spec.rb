03b8418a7f73a84677918f568e3f539d4c97cd6d
Cori Snedecor <coriannas@yahoo.com>
2015-01-08 13:54:26 -0500

    Fixes merge conflicts

--- spec/features/admin/admin_deletes_drink_spec.rb
+++ spec/features/admin/admin_deletes_drink_spec.rb
@@ -1,39 +1,39 @@
 require "rails_helper"
 
-feature "Delete user", %{
+feature "Delete drink", %{
   As an admin,
   I want to be able to delete a drink,
   So that I can get rid of unrelated submissions
   } do
 
-  # Acceptance Criteria:
-  #
-  # [X] I must be signed in as an admin to remove a user
-  # [X] I must have a way of deleting a user
+    # Acceptance Criteria:
+    #
+    # [X] I must be signed in as an admin to remove a user
+    # [X] I must have a way of deleting a user
 
-  context "Admin is signed in" do
-    before(:each) do
-      @admin1 = FactoryGirl.create(:user, role: "admin")
-      visit root_path
+    context "Admin is signed in" do
+      before(:each) do
+        @admin1 = FactoryGirl.create(:user, role: "admin")
+        visit root_path
 
-      click_on "Sign In"
+        click_on "Sign In"
 
-      fill_in "Login", with: @admin1.email
-      fill_in "Password", with: @admin1.password
-      click_on "Log in"
-    end
+        fill_in "Login", with: @admin1.email
+        fill_in "Password", with: @admin1.password
+        click_on "Log in"
+      end
 
-    scenario "Admin successfully deletes a drink" do
-      drink1 = FactoryGirl.create(:drink)
-      drink2 = FactoryGirl.create(:drink, name: "drink to be destroyed")
+      scenario "Admin successfully deletes a drink" do
+        drink1 = FactoryGirl.create(:drink)
+        drink2 = FactoryGirl.create(:drink, name: "drink to be destroyed")
 
-      visit admin_drinks_path
+        visit admin_drinks_path
 
-      click_button ("Destroy " + drink2.name)
+        click_button ("Destroy " + drink2.name)
 
-      expect(page).to have_content "Successfully deleted #{drink2.name}"
-      expect(page).to have_content drink1.name
-      expect("#drinks").not_to have_content drink2.name
+        expect(page).to have_content "Successfully deleted #{drink2.name}"
+        expect(page).to have_content drink1.name
+        expect("#drinks").not_to have_content drink2.name
+      end
     end
   end
-end
