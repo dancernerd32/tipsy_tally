b3b7e049481f1ba942ca9b3115e701a450b8a611
graffam <graffam87@gmail.com>
2015-01-08 00:25:35 -0500

    adds model tests for votes, adds review model test to ensure votes are dependent destroy

---
+++ spec/models/vote_spec.rb
@@ -0,0 +1,17 @@
+require "spec_helper"
+
+describe Vote do
+  let(:blank_values) { [nil, ""] }
+
+  it { should have_valid(:score).when(1,-1,0) }
+  it { should_not have_valid(:score).when(*blank_values) }
+
+  it { should have_valid(:user_id).when(1) }
+  it { should_not have_valid(:user_id).when(*blanks) }
+
+  it { should have_valid(:review_id).when(1) }
+  it { should_not have_valid(:review_id).when(*blanks) }
+
+  it { should belong_to(:user) }
+  it { should belong_to(:review)}
+end
