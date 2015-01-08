6701162a2e9c591f2734fc64ece36427bf2f3731
graffam <graffam87@gmail.com>
2015-01-08 00:31:59 -0500

    adds validations for votes, passes tests

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,4 +1,10 @@
 class Vote < ActiveRecord::Base
    belongs_to :user
    belongs_to :review
+
+   validates :review_id,
+             :user_id,
+             :score,
+             presence: true
+
 end
