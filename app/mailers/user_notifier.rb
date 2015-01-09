34f2c44ba164784d8a14a4541c5cd1ee180f05af
Michael Foster <michael.foster@outlook.com>
2015-01-09 15:29:09 -0500

    Removes unused code

--- app/mailers/user_notifier.rb
+++
@@ -1,9 +0,0 @@
-class UserNotifier < ApplicationMailer
-	default from: "michael.foster@outlook.com",
-		return_path: "michael.foster@outlook.com"
-	@user = user
-	mail(to: @user.email, subject: "Thanks for signing up for our amazing app")
-	end
-end
-
-
