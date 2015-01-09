77b47e60a7ed4208a4c1d27f44b67278d7e0b956
Michael Foster <michael.foster@outlook.com>
2015-01-09 15:26:24 -0500

    Addresses HoundCI issues

--- app/mailers/user_notifier.rb
+++ app/mailers/user_notifier.rb
@@ -1,13 +1,9 @@
 class UserNotifier < ApplicationMailer
-	  default from: 'michael.foster@outlook.com',
-	  	return_path: 'michael.foster@outlook.com'
-
-  # send a signup email to the user, pass in the user object that   contains the user's email address
-  def send_signup_email(user)
-    @user = user
-    mail( to: @user.email,
-    subject: 'Thanks for signing up for our amazing app' )
-  end
+	default from: "michael.foster@outlook.com",
+		return_path: "michael.foster@outlook.com"
+	@user = user
+	mail(to: @user.email, subject: "Thanks for signing up for our amazing app")
+	end
 end
 
 
